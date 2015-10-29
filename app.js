2ff5881b29f42323e91ffc71a5f50a968a998ee6
Bertiana <bertiana.balliu@studenti.unitn.it>
2015-10-29 16:03:21 +0100

    memory game final_files

--- app.js
+++ app.js
@@ -5,14 +5,42 @@
 // done: when the user has discovered the pair of cards
 // hidden: when the card has not been discovered yet
 
+tmpl = "<li id='0' class='hidden'>" + "<h3>1</h3>";
+
 $(document).ready(function(){
-
-  
+  for (var i =0; i<data.length; i++){
+        item = tmpl.replace(/1/g, data[i]);
+        item = item.replace('hidden', 'done');
+        $(".cards").append(item);
+    }
+    
+    $(".opt-start").click(function(){
+        $(".cards").children().removeClass('done');
+        $(".cards").children().addClass('hidden');
+        $('h3').hide();
+    
+    });
+    
+    
 });
-
-
-
-
-
-
+var previous = 0;
+    $(document).on("click", "li", function(event){
+        previous++;
+        
+        var number = $(event.currentTarget).find("h3").html();
+        if(parseInt(number)!==previous){
+                        alert("you lost");
+        } else{ 
+                $(event.currentTarget).removeClass("hidden");
+                $(event.currentTarget).addClass("done");
+                $(event.currentTarget).find("h3").show();
+            if(parseInt(number)===9){
+                alert("cooool you're almost done");
+            }
+            if(parseInt(number)===10){
+                alert("you won!");
+            }
+        }
+    
+});
 
